adaf4c45145e5a3d853966a2d605206ef3d1dd2f
John Niyontwali <nijohn0006@gmail.com>
2022-06-10 16:56:26 +0200

    fixed a bug in the get started button

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Btn = () => {
   return (
     <div>
-      <Link to="/flashcard">
+      <Link to="/flashcards">
         <button className="relative inline-flex items-center px-8 py-2 overflow-hidden text-md font-bold text-[#ec735e]  outline outline-offset-0 outline-[#ec735e] border-[#ec735e] rounded-full hover:text-white  group">
           <span className="absolute left-0 block w-full h-0 transition-all bg-[#ec735e] group-hover:h-full group-hover:top-0 duration-400 ease"></span>
           <span className="absolute right-0 flex items-center justify-center w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
